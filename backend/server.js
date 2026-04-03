0967803be99cee2efbf4a6c0d9b7dbce29c79e48
Arpit Gupta <arpitgupta0406@gmail.com>
2026-04-03 18:26:54 +0530

    models controllers and backend actual setup has been started

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
-import path, { dirname } from "path"
-import authRoutes from "./src/routes/auth.route.js"
-import messageRoutes from "./src/routes/message.route.js"
-dotenv.config()
+import path, { dirname } from "path";
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+import { connectDB } from "./src/lib/db.js";
+dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT||3000
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if(process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "../frontend/dist")))
-app.get("*", (req,res) =>{
-  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-app.listen(PORT, () => console.log("Server is running on port :", PORT));
+app.listen(PORT, () => {
+  console.log("Server is running on port :", PORT);
+  connectDB();
+});
