d3ebd698a3b69370a6c72f0a03eb024fc690ece7
Arpit Gupta <arpitgupta0406@gmail.com>
2026-05-13 23:31:28 +0530

    backend has been setuped fronted start

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function ChatPage() {
+  
+
+  
+  return (
+    <div>
+      ChatPage
+    </div>
+  ) 
+}
+
+export default ChatPage
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+function LoginPage() {
+
+  
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default LoginPage
